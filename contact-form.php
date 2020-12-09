959a069ebf7c1714712e933b437d01783ba1af40
limyeonwoo <54699644+limyeonwoo@users.noreply.github.com>
2020-12-09 21:26:49 +0900

    Add files via upload

--- contact-form.php
+++ contact-form.php
@@ -1,7 +1,7 @@
 <?php
 // Configure your Subject Prefix and Recipient here
 $subjectPrefix = '[Contact via website]';
-$emailTo       = 'contact@yourr-domain.com';
+$emailTo       = 'lim_yeonwoo@naver.com';
 
 $errors = array(); // array to hold validation errors
 $data   = array(); // array to pass back data
